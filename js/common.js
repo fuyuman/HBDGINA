b5a0c05077c7b624d36ed5203834385974876341
Seonhu Park <86402212+pshpark@users.noreply.github.com>
2024-04-06 14:12:57 +0900

    push hashtag modify

--- js/common.js
+++ js/common.js
@@ -1,11 +1,3 @@
 window.addEventListener("load",function(){
-    const btnTag = document.querySelectorAll(".tag_wrap ul li");
-    btnTag.forEach((item) => {
-        item.addEventListener("click",function(){
-            btnTag.forEach((all) => {
-                all.classList.remove("on");
-            })
-            this.classList.toggle("on");
-        })
-    })
+
 })
